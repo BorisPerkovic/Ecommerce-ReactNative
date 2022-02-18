96a6a4203b949ccffac5c4922e44dd1c4bfc7ceb
BorisPerkovic <borisperkovic89@yahoo.com>
2022-02-18 16:47:35 +0100

    Created launch icon and splash screen, feature not finished

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
-  return <SafeAreaView />;
-};
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
-const styles = StyleSheet.create({});
+  return (
+    <SafeAreaView>
+      <Text>Loaded</Text>
+    </SafeAreaView>
+  );
+};
 
 export default App;

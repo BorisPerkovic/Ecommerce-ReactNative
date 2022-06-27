555e131ae02aed75fafd508acfeab96acf6faeb7
boris.perkovic@p3-group.com <boris.perkovic@p3-group.com>
2022-06-27 16:48:06 +0200

    Call support added

--- src/account/CallSupportNavigationItem.tsx
+++ src/account/CallSupportNavigationItem.tsx
@@ -2,6 +2,7 @@ import {AccountNavigationItem} from './AccountNavigationItem';
 import React, {FunctionComponent} from 'react';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import {useAppTheme} from '../theme';
+import {Linking} from 'react-native';
 
 export const CallSupportNavigationItem: FunctionComponent = props => {
   const {children} = props;
@@ -15,7 +16,7 @@ export const CallSupportNavigationItem: FunctionComponent = props => {
         <SimpleLineIcons name="phone" size={25} color={sideMenuTextColor} />
       }
       onPress={() => {
-        console.log('Phone Clicked');
+        Linking.openURL('tel: +000000000');
       }}>
       {children}
     </AccountNavigationItem>

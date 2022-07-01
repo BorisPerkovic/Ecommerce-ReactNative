15014c1c4e50cd867539908d57e6f8f649950843
boris.perkovic@p3-group.com <boris.perkovic@p3-group.com>
2022-07-01 16:36:19 +0200

    Device Internet connection added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import {Platform, UIManager} from 'react-native';
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import {Platform, StyleSheet, UIManager, View} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 import {NavigationContainer} from '@react-navigation/native';
 import {enableScreens} from 'react-native-screens';
 import {SafeAreaProvider} from 'react-native-safe-area-view';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Modal, Provider as PaperProvider} from 'react-native-paper';
 import {RootStack} from './RootStack';
 import FlashMessage from 'react-native-flash-message';
 import {ECFlashMessage} from './components/ECFlashMessage';
-import {ThemeContext} from './theme/theme';
+import {ThemeContext, useAppTheme} from './theme/theme';
 import {useTheme} from './theme/useTheme';
+import {ECText} from './components/ECText';
+import {useTranslation} from 'react-i18next';
 
 enableScreens();
 
@@ -20,7 +23,17 @@ if (
 }
 
 const App = () => {
+  const [isConnected, setIsConnected] = useState<boolean | null>(true);
   const {appTheme} = useTheme();
+
+  useEffect(() => {
+    const removeNetInfoSubscription = NetInfo.addEventListener(state => {
+      const online = state.isConnected && state.isInternetReachable;
+      setIsConnected(online);
+    });
+
+    return () => removeNetInfoSubscription();
+  }, []);
 
   return (
     <>
@@ -36,9 +49,52 @@ const App = () => {
             />
           </PaperProvider>
         </SafeAreaProvider>
+        <NoInternetModal isVisible={isConnected} />
       </ThemeContext.Provider>
     </>
   );
 };
 
 export default App;
+
+interface NoInternetModalProps {
+  isVisible: boolean | null;
+}
+
+const NoInternetModal: FunctionComponent<NoInternetModalProps> = ({
+  isVisible,
+}) => {
+  const {
+    colors: {backgroundColor},
+  } = useAppTheme();
+  const {t} = useTranslation('account');
+  return (
+    <Modal visible={!isVisible} style={styles.modal} dismissable={false}>
+      <View style={[styles.modalContainer, {backgroundColor: backgroundColor}]}>
+        <ECText fontSize={22} bold>
+          {t('connError')}
+        </ECText>
+        <ECText fontSize={18} textAlign="center" style={styles.modalText}>
+          {t('noInternet')}
+        </ECText>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 40,
+    alignItems: 'center',
+  },
+  modalText: {
+    marginTop: 14,
+    marginBottom: 10,
+  },
+});
